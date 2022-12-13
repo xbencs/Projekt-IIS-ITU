2f19d28d7e087b9103c9afbd8b855accaf0d574f
xcsalo00 <jasminacsalova@gmail.com>
2022-12-13 21:12:14 +0100

    search from show.blades deleted

--- resources/views/listings/show.blade.php
+++ resources/views/listings/show.blade.php
@@ -1,7 +1,5 @@
 
-<x-layout>
-    @include('partials._search')
-        
+<x-layout>        
     <a href="/" class="inline-block text-black ml-4 mb-4"
     ><i class="fa-solid fa-arrow-left"></i> Back
     </a>

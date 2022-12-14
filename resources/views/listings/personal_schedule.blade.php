35a6ecf4ec20cd6692a7ca2c6bc07f8f1eff8e84
xcsalo00 <jasminacsalova@gmail.com>
2022-12-14 20:00:52 +0100

    My schedule edit

--- resources/views/listings/personal_schedule.blade.php
+++ resources/views/listings/personal_schedule.blade.php
@@ -1,19 +1,70 @@
 {{--Created by Jasmína Csalova--}}
+
 <x-layout>
+    <x-card class p-10>
+        <header>
+            <h1
+                class="text-3xl text-center font-bold my-6 uppercase"
+            >
+                My schedule
+            </h1>
+        </header>
 
-    @include('partials._hero')
+        <table class="w-full table-auto rounded-sm">
+            <tbody>
+                @unless(count($listings) == 0)
+                @foreach($listings as $listing)
+                <tr class="border-gray-300">
+                    <td
+                        class="font-bold px-4 py-8 border-t border-b border-gray-300 text-lg"
+                    >
+                        <a href="/listings/{{$listing->id}}">
+                            {{$listing->title}}
+                        </a>
 
+                    </td>
+                    <td
+                        class="px-4 py-8 border-t border-b border-gray-300 text-lg"
+                    > <i class="fas fa-volleyball-ball"></i>
+                        <a  class="px-6 py-2 rounded-xl">
+                            {{$listing->sport}}
+                        </a>
+                    </td>
+                    <td
+                        class="px-4 py-8 border-t border-b border-gray-300 text-lg"
+                    >
+                        <i class="fas fa-calendar"></i>
+                            @if ($listing->date != NULL)
+                                <a  class="px-6 py-2 rounded-xl">
+                                    {{$listing->date}}
+                                </a>
+                            @else 
+                                <a class="px-6 py-2 rounded-xl">
+                                    Date not announced
+                                </a>
+                            @endif
+                    </td>
 
-    <div class="lg:grid lg:grid-cols-2 gap-4 space-y-4 md:space-y-0 mx-4">
+                    <td
+                        class="px-4 py-8 border-t border-b border-gray-300 text-lg"
+                    >
+                    <i class="fa-solid fa-location-dot"></i>
+                        <a  class="px-6 py-2 rounded-xl">
+                            {{$listing->location}}
+                        </a>
+                    </td>
 
-        @unless(count($listings) == 0)
-            @foreach($listings as $listing)
-                <x-listing-card :listing="$listing" />
-            @endforeach
-        @else 
-            <p>No tournaments to show.</p>
-        @endunless
-    </div>
-
+                </tr>
+                @endforeach
+                @else 
+                <tr class="border-gray-300">
+                    <td class="px-4 py-8 border-t border-b border-gray-300 trxt-lg">
+                        <p class="text-center">No Listings Found</p>
+                    </td>
+                </tr>
+                @endunless
+            </tbody>
+        </table>
+    </x-card>
 
 </x-layout>
